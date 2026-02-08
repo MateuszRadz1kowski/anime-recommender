11d59d51c4e227fd80f840443bdc5eb09bdba42d
Radzik69 <radzikowski.mateusz@gmail.com>
2026-02-08 14:46:43 +0100

    showing: why anime is recommended

--- backend/app/anime_profile/create_anime_profile.py
+++ backend/app/anime_profile/create_anime_profile.py
@@ -19,22 +19,25 @@ def create_anime_profile(db_response,user_interests_profile):
             continue
 
         if not (check_if_adult(anime) and check_format(anime) and check_season_year(anime) and check_show_planning(anime, anime_planning)):continue
+        anime_profile[anime_name] = {
+            "score": 0,
+            "why_recommended": {}
+        }
 
         for tag in anime[7]:
             tag_name = tag["name"]
             tag_rank = tag["rank"]
             if tag_name in user_interests_profile[0]:
-                if anime_name not in anime_profile:
-                    anime_profile[anime_name] = 0
-                else:
-                    anime_score += tag_rank * user_interests_profile[0][tag_name]
+                value = tag_rank * user_interests_profile[0][tag_name]
+
+                anime_score += value
+
+                anime_profile[anime_name]["why_recommended"].setdefault(tag_name, 0)
+                anime_profile[anime_name]["why_recommended"][tag_name] += value
 
         for genre in anime[6]:
             if genre in user_interests_profile[1]:
-                if anime_name not in anime_profile:
-                    anime_profile[anime_name] = 0
-                else:
-                    anime_score += ANIME_PROFILE_GENRE_MODIFIER * user_interests_profile[1][genre]
+                anime_score += ANIME_PROFILE_GENRE_MODIFIER * user_interests_profile[1][genre]
 
         if anime_name in anime_planning:
             anime_score *= ANIME_USER_PLANNING_MULTIPLIER
@@ -43,15 +46,15 @@ def create_anime_profile(db_response,user_interests_profile):
             anime_score += ANIME_PROFILE_API_RECOMMENDATIONS_MODIFIER * user_interests_profile[2][anime_name]
 
         if anime[11] is not None:
-            anime_score * mean_score_multiplier(anime[11])
-        anime_score * anime_favourites_multiplier(anime[10])
+            anime_score *= mean_score_multiplier(anime[11])
+        anime_score *= anime_favourites_multiplier(anime[10])
 
-        anime_profile[anime_name] = anime_score
+        anime_profile[anime_name]["score"] = anime_score
     normalise_score(anime_profile)
     sorted_anime_profile = dict(
         sorted(
             anime_profile.items(),
-            key=lambda x: x[1],
+            key=lambda x: x[1]["score"],
             reverse=True
         )
     )
@@ -61,11 +64,11 @@ def normalise_score(anime):
     sum_sq = 0.0
 
     for value in anime.values():
-        sum_sq += value ** 2
+        sum_sq += value["score"] ** 2
 
     norm = math.sqrt(sum_sq)
 
     for key in anime:
-        anime[key] = anime[key] / norm
+        anime[key]["score"] = anime[key]["score"] / norm
 
     return anime
